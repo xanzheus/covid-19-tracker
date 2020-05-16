01364727fe192ae5af85c1f640f3196eb307f7a4
Yossan Rahmadi <yossansr06@gmail.com>
2020-05-16 17:20:18 +0800

    Updates

---
+++ precache-manifest.7f529a0a99d600343569091fe24dabbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4968e87f953dcb194cde7afbce8d03",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "13f5648890e41a2c80e4",
+    "url": "/covid-19-tracker/static/css/main.066de84c.chunk.css"
+  },
+  {
+    "revision": "afeb8d34e0265e99e684",
+    "url": "/covid-19-tracker/static/js/2.bed3352a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.bed3352a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f5648890e41a2c80e4",
+    "url": "/covid-19-tracker/static/js/main.8c1bad60.chunk.js"
+  },
+  {
+    "revision": "fa0c104a82072024f0ae",
+    "url": "/covid-19-tracker/static/js/runtime-main.5b571555.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
